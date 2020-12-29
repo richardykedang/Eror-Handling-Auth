79d05e5166aaffd6729f713d5cce910878bc6ed7
richardykedang <kedangrichard@gmail.com>
2020-12-29 14:21:33 +0700

    Refactoring Error Handling

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
 const { get } = require('./router/tourRoute');
+const appError = require('./utils/appError');
+const globalErrorHandler = require('./Controller/errorController');
 
-const tourRouter = require('./router/tourRoute')
+const tourRouter = require('./router/tourRoute');
 
 const app = express();
 
@@ -17,26 +19,10 @@ app.use('/api/v1/tours', tourRouter)
 //console.log(tours)
 //Handling unhandled routes
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `cant find ${req.originalUrl} on this server`
-    // })
-    const err = new Error(`cant find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
-    next(err)
+     next(new appError (`cant find ${req.originalUrl} on this server`, 404))
 });
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-
-})
+app.use(globalErrorHandler);
 
 //4. SERVER
 
