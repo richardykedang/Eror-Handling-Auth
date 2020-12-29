fcf0e3defe49ec79becaa7463fd7afa790716cb7
richardykedang <kedangrichard@gmail.com>
2020-12-29 16:43:57 +0700

    Managing password

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,7 +1,7 @@
 const User = require('../Model/userModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.signup = catchAsync(async (req,res) => {
+exports.signup = catchAsync(async (req,res,next) => {
 
         const newUser = await User.create(req.body);
         
--- Model/userModel.js
+++ Model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name, email, password
 const userSchema = new mongoose.Schema({
@@ -24,6 +25,13 @@ const userSchema = new mongoose.Schema({
     
 });
 
+userSchema.pre("save", async function(next) {
+    //only run if paswword actually modified
+    if(!this.isModified('password')) return next();
+
+    //hash the password
+    this.password = await bcrypt.hash(this.password, 12);
+})
 
 const User = mongoose.model('User',userSchema);
 
