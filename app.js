7171b3d4f1c22e77d2b462b9bd6a7ec74b903d1d
richardykedang <kedangrichard@gmail.com>
2020-12-29 13:50:31 +0700

    Handling unhadled routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
+const { get } = require('./router/tourRoute');
 
 const tourRouter = require('./router/tourRoute')
 
@@ -14,6 +15,28 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter)
 
 //console.log(tours)
+//Handling unhandled routes
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `cant find ${req.originalUrl} on this server`
+    // })
+    const err = new Error(`cant find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err)
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+
+})
 
 //4. SERVER
 
